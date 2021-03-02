924e53ae2bbdae7562a584c3ad29658a8e50b922
DESKTOP-HPKO9NR\hanmi <hanmi8080@gmail.com>
2021-03-02 14:08:38 +0900

    서브페이지

--- js/common.js
+++ js/common.js
@@ -6,10 +6,35 @@ $(function(){
         $('.container').toggleClass('menu-off');
     })
 
-    $('.menu').children('li').children('a').click(function(){
+    $('.menu').children('li').children('a').click(function() {
         $('.menu').children('li').removeClass('on');
         $(this).parent('li').addClass('on');
-        $(this).children('.arrow').toggleClass('fa-caret-down fa-caret-up')
-        $(this).next('.menu-second').slideToggle(200);
+        $(this).children('i').toggleClass('fa-caret-down fa-caret-up')
+        if($(this).next('.menu-second').hasClass('on')){
+            $(this).next('.menu-second').slideUp(200);
+        }else{
+            $(this).next('.menu-second').slideDown(200);
+        }
+        $(this).next('.menu-second').toggleClass('on')
+
+    })
+
+    $('.tabs').find('a').click(function(e){
+        e.preventDefault();
+       var tabID = $(this).attr('href');
+       $('.tab-contents').hide();
+       $(tabID).show();
+       $(this).parent('li').siblings('li').removeClass('on');
+       $(this).parent('li').addClass('on');
+    });
+
+    $('.modalCall').click(function(){
+        var modalID = $(this).attr('name');
+        $('body').css('overflow', 'hidden');
+        $('#' + modalID).show();
+    })
+    $('.modal-close').click(function(){
+        $('body').css('overflow', '');
+        $(this).parents('.modal').hide();
     })
 })
