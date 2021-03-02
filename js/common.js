89f9ac49726115e61eecb3fe08b0dbd23278d3e0
DESKTOP-HPKO9NR\hanmi <hanmi8080@gmail.com>
2021-03-02 20:10:08 +0900

    반응형

--- js/common.js
+++ js/common.js
@@ -6,6 +6,24 @@ $(function(){
         $('.container').toggleClass('menu-off');
     })
 
+    if($(window).width() < 1024) {
+        $('.menu-wrap').addClass('menu-off');
+        $('.container').addClass('menu-off');
+    }else{
+        $('.menu-wrap').removeClass('menu-off');
+        $('.container').removeClass('menu-off');
+    }
+    $(window).resize(function(){
+        if($(window).width() < 1024) {
+            $('.menu-wrap').addClass('menu-off');
+            $('.container').addClass('menu-off');
+        }else{
+            $('.menu-wrap').removeClass('menu-off');
+            $('.container').removeClass('menu-off');
+        }
+
+    })
+
     $('.menu').children('li').children('a').click(function() {
         $('.menu').children('li').removeClass('on');
         $(this).parent('li').addClass('on');
